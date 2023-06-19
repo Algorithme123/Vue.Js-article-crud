1ce1d6580c578063530059d523f4fa9549b8c62a
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2023-06-19 03:59:04 +0000

    getAll Ok

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,35 @@ import { RouterLink, RouterView } from 'vue-router'
 
     <div class="wrapper">
 
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink class="nav-link active" aria-current="page" to="#">Home</RouterLink>
+
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="#">Link</RouterLink>
+              </li>
+             
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/about">A propos</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/articles">Articles</RouterLink>
+              </li>
+              
+            </ul>
+         
+          </div>
+        </div>
+      </nav>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -14,5 +43,4 @@ import { RouterLink, RouterView } from 'vue-router'
     </div>
   </header>
 
-  <RouterView />
-</template>
+<RouterView /></template>
---
+++ src/views/Articles/View.vue
@@ -0,0 +1,87 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+
+    <div class="card">
+        <div class="card-header">
+            <h3>Articles</h3>
+        <RouterLink to="/articles/new" class="btn btn-primary float-end"> Ajouter Articles </RouterLink>
+
+        </div>
+
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+
+            <div class="table-responsive">
+                <table class="table table-striped
+                	
+                table-borderless
+                table-primary
+                align-middle">
+                    <thead class="table-light">
+                        <caption>Table Name</caption>
+                        <tr>
+                            <th>ID</th>
+                            <th>Code</th>
+                            <th>Slug</th>
+                            <th>libelle</th>
+                            <th>Prix</th>
+                            <th>Description</th>
+                            <th>date</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody v-if="this.articles.length > 0" class="table-group-divider">
+                            <tr v-for="(article,index) in this.articles" :key="index"  class="table-primary" >
+                                <td scope="row">{{index +1}}</td>
+                                <td>{{article.code}}</td>
+                                <td>{{article.libelle}}</td>
+                                <td>{{article.slug}}</td>
+                                <td>{{article.description}}</td>
+                                <td>{{article.prix}}</td>
+                                <td>{{article.created_at}}</td>
+                                <td>
+                                    <RouterLink to="/" type="button" class="btn btn-success"> Edit</RouterLink>
+                                    <RouterLink to="/" type="button" class="btn btn-danger"> Delete</RouterLink>
+                                </td>
+                            </tr>
+                            
+                        </tbody>
+                        <tbody v-else>
+                            <tr colspan="7"> Chargement ...</tr>
+                        </tbody>
+                        <tfoot>
+                            
+                        </tfoot>
+                </table>
+            </div>
+            
+             
+
+
+        </div>
+    </div>
+</template>
+  <script>
+  import axios from 'axios'
+  export default{
+    name: 'articles',
+    data(){
+        return {
+            articles:[]
+        }
+    },
+    mounted(){
+        this.getArticles();
+        console.log("ok")
+    },
+    methods:{   
+        getArticles(){
+            axios.get("http://127.0.0.1:8000/api/articles").then(res=>{
+                this.articles = res.data.articles
+                console.log(this.articles)
+
+            })
+        }
+    }
+  }
+</script>
